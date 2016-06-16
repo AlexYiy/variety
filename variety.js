b03b23d3bc960e21d82390410607403a3cb654ca
root <root@localhost.localdomain>
2016-06-17 03:11:52 +0800

    variety for mongo_terark

--- variety.js
+++ variety.js
@@ -18,9 +18,6 @@ Released by Maypop Inc, © 2012-2016, under the MIT License. */
     }
   };
 
-  log('Variety: A MongoDB Schema Analyzer');
-  log('Version 1.5.0, released 14 May 2015');
-
   var dbs = [];
   var emptyDbs = [];
 
@@ -69,7 +66,6 @@ Released by Maypop Inc, © 2012-2016, under the MIT License. */
     var read = function(name, defaultValue) {
       var value = typeof configProvider[name] !== 'undefined' ? configProvider[name] : defaultValue;
       config[name] = value;
-      log('Using '+name+' of ' + tojson(value));
     };
     read('collection', null);
     read('query', {});
@@ -77,7 +73,7 @@ Released by Maypop Inc, © 2012-2016, under the MIT License. */
     read('maxDepth', 99);
     read('sort', {_id: -1});
     read('outputFormat', 'ascii');
-    read('persistResults', false);
+    read('persistResults', true);
     read('resultsDatabase', 'varietyResults');
     read('resultsCollection', collection + 'Keys');
     read('resultsUser', null);
@@ -133,14 +129,17 @@ Released by Maypop Inc, © 2012-2016, under the MIT License. */
       });
     };
 
-    log('Using plugins of ' + tojson(this.plugins.map(function(plugin){return plugin.path;})));
   };
 
   var $plugins = new PluginsClass(this);
   $plugins.execute('onConfig', config);
 
   var varietyTypeOf = function(thing) {
-    if (typeof thing === 'undefined') { throw 'varietyTypeOf() requires an argument'; }
+
+    if (typeof thing === 'undefined') { 
+      return 'undefined';
+    }
+    
 
     if (typeof thing !== 'object') {
     // the messiness below capitalizes the first letter, so the output matches
@@ -159,14 +158,7 @@ Released by Maypop Inc, © 2012-2016, under the MIT License. */
       } else if (thing instanceof ObjectId) {
         return 'ObjectId';
       } else if (thing instanceof BinData) {
-        var binDataTypes = {};
-        binDataTypes[0x00] = 'generic';
-        binDataTypes[0x01] = 'function';
-        binDataTypes[0x02] = 'old';
-        binDataTypes[0x03] = 'UUID';
-        binDataTypes[0x05] = 'MD5';
-        binDataTypes[0x80] = 'user';
-        return 'BinData-' + binDataTypes[thing.subtype()];
+        return 'BinData';
       } else {
         return 'Object';
       }
@@ -214,6 +206,36 @@ Released by Maypop Inc, © 2012-2016, under the MIT License. */
     return result;
   };
 
+  var serializeDocterark = function(doc, maxDepth) {
+	  var result = {};
+
+	  //determining if an object is a Hash vs Array vs something else is hard
+	  //returns true, if object in argument may have nested objects and makes sense to analyse its content
+	  function isHash(v) {
+		  var isArray = Array.isArray(v);
+		  var isObject = typeof v === 'object';
+		  var specialObject = v instanceof Date ||
+			  v instanceof ObjectId ||
+			  v instanceof BinData;
+		  return !specialObject && (isArray || isObject);
+	  }
+
+	  function serializeterark(document, parentKey, maxDepth){
+		  for(var key in document){
+			  //skip over inherited properties such as string, length, etch
+			  if(!document.hasOwnProperty(key)) {
+				  continue;
+			  }
+			  var value = document[key];
+			  //objects are skipped here and recursed into later
+			  //if(typeof value != 'object')
+			  result[parentKey+key] = value;
+		  }
+	  }
+	  serializeterark(doc, '', maxDepth)
+	  return result;
+  };
+
   // convert document to key-value map, where value is always an array with types as plain strings
   var analyseDocument = function(document) {
     var result = {};
@@ -230,6 +252,44 @@ Released by Maypop Inc, © 2012-2016, under the MIT License. */
     return result;
   };
 
+  var analyseDocumentterark = function(document, maxDepth) {
+	  var result = {};
+
+	  var recurresult = {};
+
+	  function serializeAValue(value, maxDepth, pararesult) {
+		  for(var key in value) {
+			  pararesult[key] = {};
+			  var tempv = value[key];
+			  var type = varietyTypeOf(tempv);
+			  if(type == 'Object' && maxDepth > 1) {
+				  pararesult[key][type] = {};
+				  serializeAValue(tempv, maxDepth-1, pararesult[key][type]);
+			  } else {
+				  pararesult[key][type]=true; 
+			  }
+		  }
+	  }
+
+	  for (var key in document) {
+		  var value = document[key];
+		  //translate unnamed object key from {_parent_name_}.{_index_} to {_parent_name_}.XX
+		  key = key.replace(/\.\d+/g,'.XX');
+		  if(typeof result[key] === 'undefined') {
+			  result[key] = {};
+		  }
+		  var type = varietyTypeOf(value);
+		  if (type === "Object") {
+			  serializeAValue(value, maxDepth, recurresult);
+			  result[key][type] = recurresult;
+		  } else {
+			  result[key][type] = true;
+		  }
+	  }
+	  return result;
+  };
+
+
   var mergeDocument = function(docResult, interimResults) {
     for (var key in docResult) {
       if(key in interimResults) {
@@ -241,7 +301,7 @@ Released by Maypop Inc, © 2012-2016, under the MIT License. */
           } else {
             existing.types[type] = 1;
             if (config.logKeysContinuously) {
-              log('Found new key type "' + key + '" type "' + type + '"');
+              // log('Found new key type "' + key + '" type "' + type + '"');
             }
           }
         }
@@ -251,13 +311,126 @@ Released by Maypop Inc, © 2012-2016, under the MIT License. */
         for (var newType in docResult[key]) {
           types[newType] = 1;
           if (config.logKeysContinuously) {
-            log('Found new key type "' + key + '" type "' + newType + '"');
+            // log('Found new key type "' + key + '" type "' + newType + '"');
           }
         }
         interimResults[key] = {'types': types,'totalOccurrences':1};
       }
     }
   };
+
+  var mergeDocumentterark = function(docResultstrc, interimResultsstrc) {
+	  for (var key in docResultstrc) {
+		  if(key in interimResultsstrc) {
+			  var existing = interimResultsstrc[key];
+
+			  for(var type in docResultstrc[key]) {
+				  if (type in existing.types) {
+					  existing.types[type] = existing.types[type] + 1;
+				  } else {
+					  existing.types[type] = 1;
+				  }
+			  }
+			  existing.totalOccurrences = existing.totalOccurrences + 1;
+		  } else {
+			  var types = {};
+			  for (var newType in docResultstrc[key]) {
+				  types[newType] = 1;
+			  }
+			  var value = docResultstrc[key];
+			  var temp = Object.keys(value);
+			  if (temp == 'Object') {
+				  interimResultsstrc[key] = {'types': types,'totalOccurrences':1, value};
+			  } else {
+				  interimResultsstrc[key] = {'types': types,'totalOccurrences':1};
+			  }
+		  }
+	  }
+  };
+
+  var convertSchema = function(interimResultsstrc, documentsCount, nestedschemaset, interimResults) {
+	var getKeyType = function(type) {
+		var TypeToType = {
+			"Double"	:	"Float64",	
+			"String" 	:	"StrZero",
+			"Object"	:	"Nested",
+			"Array"		: 	"Nested",
+			"BinData"	:	"CarBin",
+			"undefined"	:	"",
+			"ObjectId"	:	"Fixed",
+			"Boolean"	:	"Uint08",
+			"Date"		:	"Sint64",
+			"Null"		:	"",
+			"Regex"		:	"TwoStrZero",
+			"DBPointer"	:	"",
+			"JavaScript"	:	"",
+			"Symbol"	:	"StrZero",
+			"JSWithScope"	:	"CarBin",	
+			"Number"	:	"Sint32",  // here should be modified next
+			"Timestamp"	:	"Sint64",
+			"NumberLong"	:	"Sint64",
+			"MinKey"	:	"",
+			"MaxKey"	:	""
+		};
+		return TypeToType[type];
+	}
+	
+	var columns = {};
+	var differfield = {};
+	var nestedindex = 1;
+
+	function serializeCValue(value, pararesult) {
+		pararesult["nested"] = {};
+		for(var key in value) {
+			var tempv = value[key];
+			var type = Object.keys(tempv);
+			if(type == 'Object') {
+				pararesult["nested"][key] = {'type':getKeyType(type.toString())};
+				serializeCValue(tempv['Object'], pararesult["nested"][key]);
+			} else {
+				pararesult["nested"][key] = {'type':getKeyType(type.toString())};
+			}
+		}
+	}
+
+	for (var key in interimResultsstrc) {
+		var entry = interimResultsstrc[key];
+		var nested = {};
+		var typeKeys = Object.keys(entry.types).toString();
+		if (entry.totalOccurrences === documentsCount) {
+		  	// all the records containe the field
+			if (typeKeys === "ObjectId") {
+				columns[key] = {'type': getKeyType(typeKeys), 'length': 12};
+			} else if (typeKeys === "Object") {
+				serializeCValue(entry["value"]['Object'], nested);
+				var k = Object.keys(nested);
+				var nested = nested[k.toString()];
+				columns[key] = {'type': getKeyType(typeKeys), nested};
+			} else {
+				columns[key] = {'type': getKeyType(typeKeys)};
+			}
+		} else {
+			// just some records containe the field
+			if (typeKeys === "Object") {
+				// object structure ==> nestedschemaset
+				var tempnestedschemaset = {};
+				serializeCValue(entry["value"]['Object'], tempnestedschemaset);
+				var k = Object.keys(tempnestedschemaset);
+				tempnestedschemaset = tempnestedschemaset[k.toString()];
+				tempnestedschemaset["$$"] = {"type": "CarBin"};
+				
+				nestedschemaset[nestedindex.toString()] = {'columns': tempnestedschemaset};
+				nestedindex += 1
+			} else {
+				// not object ==> $$
+			}
+			 
+		}
+	}
+	columns["$$"] = {"type": "CarBin"};
+	return columns;
+  };
+
 
   var convertResults = function(interimResults, documentsCount) {
     var getKeys = function(obj) {
@@ -289,6 +462,13 @@ Released by Maypop Inc, © 2012-2016, under the MIT License. */
     return accumulator;
   };
 
+  var reduceDocumentsterark = function(accumulatorterark, object) {
+  var docResultterark = analyseDocumentterark(serializeDocterark(object, config.maxDepth), config.maxDepth);
+  mergeDocumentterark(docResultterark, accumulatorterark);
+  return accumulatorterark;
+};
+
+
   // We throw away keys which end in an array index, since they are not useful
   // for our analysis. (We still keep the key of their parent array, though.) -JC
   var arrayRegex = new RegExp('\\.' + config.arrayEscape + '$', 'g');
@@ -312,11 +492,82 @@ Released by Maypop Inc, © 2012-2016, under the MIT License. */
   };
 
   var cursor = db[config.collection].find(config.query).sort(config.sort).limit(config.limit);
+  var cursorterark = db[config.collection].find(config.query).sort(config.sort).limit(config.limit);
+ 
   var interimResults = cursor.reduce(reduceDocuments, {});
-  var varietyResults = convertResults(interimResults, cursor.size())
-  .filter(filter)
-  .sort(comparator);
+  var interimResultsterark = cursorterark.reduce(reduceDocumentsterark, {});
+  
+//  var varietyResults = convertResults(interimResults, cursor.size())
+//  .filter(filter)
+//  .sort(comparator);
+  var nestedschemaset = {};
+  var columns = convertSchema(interimResultsterark, cursorterark.size(), nestedschemaset, interimResults);
 
+  // this is the part of TableIndex
+  // ========================================================================
+  var originalindex = db[config.collection].getIndexes();
+  var generateIndex = function(originalindex) {
+	  var results = [];
+	  for(var index in originalindex) {
+		  if ("_id_" !== originalindex[index].name) {
+			  var field = Object.keys(originalindex[index].key);
+			  var part = [];
+			  var choice = {
+				  "1" : "",
+				  "-1": "-",
+			  }			
+			  // 1 is up, and -1 is down
+			  var ordered = true;
+			  var unique = originalindex[index].unique;
+			  if (unique !== true)
+				  unique = false;
+			  for (var key in field) {
+				  var upordown = originalindex[index].key[field[key]];
+				  if (upordown === "hashed") {
+					  part.push(field[key]);
+					  ordered = false;
+				  } else {
+					  part.push(choice[upordown] + field[key]);
+				  }
+			  }
+			  results.push({
+					  'fields':part.toString(),
+					  'ordered':ordered,
+					  'unique':unique,
+					  });
+		  } else {
+			  results.push({
+					  'fields':"_id",
+					  'ordered':true,
+					  'unique':true,
+					  });
+
+		  }
+	  }
+	  return results;
+  }
+
+  var tableindex = generateIndex(originalindex);
+  //print("tableindex " + tojson(tableindex));
+  // ========================================================================
+
+
+  // this is the result: RowSchema + TableIndex + NestedSchemaSet 
+  // ========================================================================
+  var Results = {};
+
+  Results["RowSchema"] = columns;
+  if (Object.keys(tableindex).length) {
+	Results["TableIndex"] = tableindex;
+  }
+
+  if (Object.keys(nestedschemaset).length) {
+	Results["NestedSchemaSet"] = nestedschemaset;
+  }
+
+  print(tojson(Results));
+
+/*
   if(config.persistResults) {
     var resultsDB;
     var resultsCollectionName = config.resultsCollection;
@@ -334,7 +585,7 @@ Released by Maypop Inc, © 2012-2016, under the MIT License. */
     }
 
     // replace results collection
-    log('replacing results collection: '+ resultsCollectionName);
+    // log('replacing results collection: '+ resultsCollectionName);
     resultsDB[resultsCollectionName].drop();
     resultsDB[resultsCollectionName].insert(varietyResults);
   }
@@ -372,7 +623,6 @@ Released by Maypop Inc, © 2012-2016, under the MIT License. */
     var border = '+' + pad(table[0].length - 2, '', '-') + '+';
     return [border].concat(table).concat(border).join('\n');
   };
-
   var pluginsOutput = $plugins.execute('formatResults', varietyResults);
   if (pluginsOutput.length > 0) {
     pluginsOutput.forEach(function(i){print(i);});
@@ -381,5 +631,5 @@ Released by Maypop Inc, © 2012-2016, under the MIT License. */
   } else {
     print(createAsciiTable(varietyResults)); // output nice ascii table with results
   }
-
+*/
 }.bind(this)()); // end strict mode
